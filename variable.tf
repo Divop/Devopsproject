76968d7d64bff0a33e8bcf5c5230fdc54287ed61
Divop <93809648+Divop@users.noreply.github.com>
2023-01-13 13:20:59 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -31,11 +31,6 @@ variable "sshport" {
     default = 22
 }
 
-variable "inputname" {
-    type = string
-    description = "Set the name of the vpc"
-}
-
 variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
