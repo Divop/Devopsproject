4dda104bb92166c29bd6f471f0f421dac23f57b0
Divop <93809648+Divop@users.noreply.github.com>
2023-01-13 15:14:27 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,76 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "192.168.0.0/16"
+
+  tags = {
+    Name = "Web_VPC"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.myvpc.id
+
+}
+resource "aws_route_table" "main_route" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Route1"
+  }
+}
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "192.168.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Subnet1"
+  }
+}
+  resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.main_route.id
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow Web inbound traffic"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
