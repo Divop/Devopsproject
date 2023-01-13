1aecc7f9785324cd19706a1d8e3ac9fd81502992
Divop <93809648+Divop@users.noreply.github.com>
2023-01-13 15:17:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,37 +40,3 @@ resource "aws_subnet" "subnet-1" {
   route_table_id = aws_route_table.main_route.id
 }
 
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web"
-  description = "Allow Web inbound traffic"
-  vpc_id      = aws_vpc.myvpc.id
-}
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
