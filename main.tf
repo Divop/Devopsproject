2a81778e1f953b3997a5eb4d3d2b2e0dab9cd1f7
Divop <93809648+Divop@users.noreply.github.com>
2023-01-16 19:27:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,7 @@ resource "aws_instance" "Websever1" {
 	ami = "0b5eea76982371e91"
 	instance_type = "t2.micro"	
 }
-resource "aws_instance" "Websever2" {
-	ami = "0b5eea76982371e91"
-	instance_type = "t2.micro"	
-}
+
 resource "aws_S3_bucket" "dino10bucket" {
 	bucket = "dino0101"
 	acl = "private"
